3035f9c54c8e5bfbb51fbafef9e4ecbbc0391ce5
sendak <sendak.vivian.mahr@gmail.com>
2021-01-15 00:08:17 -0800

    shows picked logos on result page

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ import clustering.cluster as cluster
 #reminder for me to remove them
 exclude_list = [
 1176,	# teamviewer
-6536    #merrell_(company)
+6536,    #merrell_(company)
+2199, 	#pukka_herbs
+4291, 	#badedas
+1479,	#broughton_foods_company
+1926, 	#killer_shake
+4639,	#moka_pot
 ]
 num_logos_show = 20
 num_closest_logos = 5
@@ -35,6 +40,7 @@ def random_logo(exclude=[]):
 		"coord": strip_list(cluster.get_logo_arguments(logo_id)),
 		"id": logo_id
 	}
+
 def average_id_coords(ids):
 	ids = [cluster.DATA[i] for i in ids]
 	result = []
@@ -56,12 +62,14 @@ def random_logos(n, exclude=[]):
 		result.append(logo)
 	return result
 
-def load_cluster_images(cluster_id):
+def load_cluster_images(cluster_id, exclude=[]):
 	"""Given cluster ID, returns the images of logos in that cluster"""
+	exclude += exclude_list
 	ids_ = cluster.CLUSTERS[cluster_id]
 	result = []
 	for i in ids_:
-		result.append(logo_id_to_image(i))
+		if i not in exclude:
+			result.append(logo_id_to_image(i))
 	return result
 
 @app.route("/")
@@ -84,24 +92,30 @@ def get_final_url():
 	ids = request.args.get('ids').split(",")
 	ids = [int(i) for i in ids]
 	clusters = [cluster.id_to_cluster(i) for i in ids]
+	final_average = average_id_coords(ids)
 	final_cluster = -1
 	for id_ in clusters:
-		if clusters.count(id_) >= 3:
+		if clusters.count(id_) >= int(len(clusters) * .3):
 			clusters = id_
 	if final_cluster == -1:
-		final_average = average_id_coords(ids)
 		final_cluster = cluster.predict_coord(final_average)
 	closest_logos = cluster.closest_logos(final_average, 3)
-	return "/results?cluster={}&id={}".format(final_cluster, strip_list(closest_logos))
+	return "/results?cluster={}&id={}&chosen={}".format(final_cluster, strip_list(closest_logos), request.args.get('ids'))
 
 # TODO - descriptions of each cluster, parse ids of the 3 closest logos in url
 @app.route("/results")
 def results():
+	chosen_logos = request.args.get('chosen').split(",")
+	remove_from_cluster_ids = [int(i) for i in chosen_logos]
+	chosen_logos = [logo_id_to_image(int(i)) for i in chosen_logos]
+
 	cluster_id = int(request.args.get('cluster'))
 	closest_logos = request.args.get('id').split(",")
+	remove_from_cluster_ids += [int(i) for i in closest_logos]
 	closest_logos = [logo_id_to_image(int(i)) for i in closest_logos]
-	cluster_logos = load_cluster_images(cluster_id)
+	cluster_logos = load_cluster_images(cluster_id, remove_from_cluster_ids)
 	return render_template("results.html",	
+		chosen_logos=chosen_logos,
 		closest_logos=closest_logos,
 		cluster_logos=cluster_logos,
 		cluster_id=cluster_id
